992b3f286eafc614dfc267a4e2ec3cd9acc344e1
Tarun Behera <starbehera6@gmail.com>
2024-10-25 00:01:31 +0530

    automatic added

--- background.js
+++ background.js
@@ -1,11 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
-  // Nothing happens on installation
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  // Inject the content script only when the action button is clicked
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
+    chrome.action.onClicked.addListener((tab) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content.js']
+      });
+    });
   });
-});
+  
